5f9adcb8e86310fbd9ba6abe5b753ee776dc8069
ambsenyestudi <adrifre7@hotmail.com>
2021-02-06 11:32:26 +0100

    Course Should have valid iso codes

--- src/EasyLanguageLearning/Courses.Domain/CourseAggregate.cs
+++ src/EasyLanguageLearning/Courses.Domain/CourseAggregate.cs
@@ -5,16 +5,20 @@ namespace Courses.Domain
     public class CourseAggregate
     {
         private const string DuplicatedLanguageError = "Learning language cannot be the same as mother language";
+        private const string InvalidLanguageError = "Invalid language Iso";
         public Course ChooseACourse(string motherLanguageIsoRaw, string leaningLanguageIsoRaw)
         {
             var motherIso = IsoFromRaw(motherLanguageIsoRaw);
             var learningIso = IsoFromRaw(leaningLanguageIsoRaw);
-            if(motherIso == learningIso)
+
+            if (motherIso == learningIso)
             {
                 throw new ArgumentException(DuplicatedLanguageError);
             }
+
             return new Course(motherIso, learningIso);
         }
+
         private IsoCodes ParseIsoCode(string isoCodeRaw)
         {
             if(Enum.TryParse<IsoCodes>(isoCodeRaw, out IsoCodes parsedIso))
@@ -26,7 +30,11 @@ namespace Courses.Domain
         private Iso IsoFromRaw(string isoCodeRaw)
         {
             var isoCode = ParseIsoCode(isoCodeRaw);
-            return Iso.CreateIso(isoCode);
+            var result = Iso.CreateIso(isoCode);
+
+            return result == Iso.Empty
+                ? throw new ArgumentException(InvalidLanguageError)
+                : result;
         }
     }
 }
